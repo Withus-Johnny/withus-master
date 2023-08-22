d241dd9e6c394f4c9399dd7e6c790a8984752880
Withus-Johnny <withus.johnny@gmail.com>
2023-08-22 10:07:42 +0900

    [Client] 회원가입 폼 업데이트 - 중복 및 테스트 코드 제거

--- Withus/Client/Forms/RegisterForm.cs
+++ Withus/Client/Forms/RegisterForm.cs
@@ -8,10 +8,6 @@ namespace Client.Forms
 {
     public partial class RegisterForm : Form
     {
-        private Timer _fadeTimer = new Timer();
-        private const int FADE_TICK_VALUE = 25;
-        private const double FADE_OPACITY_VALUE = 0.050;
-
         private Rectangle _borderRect;
 
         private bool _isDraging = false;
@@ -20,11 +16,9 @@ namespace Client.Forms
         private bool _isDragging = false;
 
         private const int GRIP_SIZE = 15;
-        private Rectangle gripRect;
+        private Rectangle _gripRect;
         private bool _isResizing = false;
         private Point _lastMousePosition;
-
-        private bool _isFading = false;
 
         public RegisterForm()
         {
@@ -35,14 +29,13 @@ namespace Client.Forms
             this.BackColor = Colors.FormBackGroundColor;
             this.Padding = new Padding(Const.RegisterForm_Padding);
             _borderRect = new Rectangle(ClientRectangle.Location, new Size(ClientRectangle.Width - 1, ClientRectangle.Height - 1));
-            gripRect = new Rectangle(this.Width - GRIP_SIZE, this.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
+            _gripRect = new Rectangle(this.Width - GRIP_SIZE, this.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
 
             this.SetStyle(ControlStyles.DoubleBuffer, true);
             this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             this.SetStyle(ControlStyles.UserPaint, true);
 
-            this.Load += RegisterForm_Load;
-            this.Activated += RegisterForm_Activated;
+
 
             panel_Container.MouseDown += Panel_DragMouseDown;
             panel_Container.MouseMove += Panel_DragMouseMove;
@@ -53,161 +46,18 @@ namespace Client.Forms
             this.MouseUp += Form_MouseUp;
         }
 
-        #region Functions
 
-        private void InitializeFadeIn()
-        {
-            this.WindowState = FormWindowState.Normal;
-            _isFading = true;
-            _fadeTimer.Interval = FADE_TICK_VALUE;
-            _fadeTimer.Tick += FadeInTimer_Tick;
-            _fadeTimer.Start();
-        }
-        private void InitializeFadeOut(Action callBackAction)
-        {
-            _isFading = true;
-            _fadeTimer.Interval = FADE_TICK_VALUE;
-            _fadeTimer.Tag = callBackAction;
-            _fadeTimer.Tick += FadeOutTimer_Tick;
-            _fadeTimer.Start();
-        }
-
-        private async Task FlashBorder(int duration = 150, int repeatCount = 3)
-        {
-            using (Graphics g = this.CreateGraphics())
-            {
-                g.ResetClip();
-
-                for (int i = 0; i < repeatCount; i++)
-                {
-                    using (Pen pen = new Pen(Color.IndianRed))
-                    {
-                        g.DrawRectangle(pen, _borderRect);
-                    }
-                    await Task.Delay(duration);
-
-                    using (Pen pen = new Pen(Colors.FormBorderColor))
-                    {
-                        g.DrawRectangle(pen, _borderRect);
-                    }
-                    await Task.Delay(duration);
-                }
-            }
-        }
-
-        private async void EnsureWindowPositionWithinScreenBounds()
-        {
-            // 현재 창이 속한 스크린을 가져옵니다.
-            Screen currentScreen = Screen.FromControl(this);
-
-            // 스크린의 작업 영역을 가져옵니다.
-            Rectangle screenBounds = currentScreen.WorkingArea;
-
-            // 창이 화면 영역 밖으로 나가는지 여부를 나타내는 변수입니다.
-            bool outOfBounds = false;
-
-            // 현재 창의 위치를 새 위치로 업데이트할 변수를 생성합니다.
-            Point newLocation = this.Location;
-
-            // 창이 화면의 왼쪽 경계를 벗어난 경우 처리합니다.
-            if (this.Left < screenBounds.Left)
-            {
-                newLocation.X = screenBounds.Left;
-                outOfBounds = true;
-            }
-
-            // 창이 화면의 오른쪽 경계를 벗어난 경우 처리합니다.
-            if (this.Right > screenBounds.Right)
-            {
-                newLocation.X = screenBounds.Right - this.Width;
-                outOfBounds = true;
-            }
-
-            // 창이 화면의 상단 경계를 벗어난 경우 처리합니다.
-            if (this.Top < screenBounds.Top)
-            {
-                newLocation.Y = screenBounds.Top;
-                outOfBounds = true;
-            }
-
-            // 창이 화면의 아래쪽 경계를 벗어난 경우 처리합니다.
-            if (this.Bottom > screenBounds.Bottom)
-            {
-                newLocation.Y = screenBounds.Bottom - this.Height;
-                outOfBounds = true;
-            }
-
-            // 창이 화면 영역을 벗어났다면 새 위치로 이동합니다.
-            if (outOfBounds)
-            {
-                this.Location = newLocation;
-                await FlashBorder();
-            }
-        }
-        #endregion
 
         #region Control Events
-        private void RegisterForm_Load(object sender, EventArgs e)
-        {
-            // Fade In 처리를 위한 사전 설정
-            this.Opacity = 0;
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void RegisterForm_Activated(object sender, EventArgs e)
-        {
-            InitializeFadeIn();
-        }
-        private void FadeInTimer_Tick(object sender, EventArgs e)
-        {
-            if (this.Opacity == 1)
-            {
-                _fadeTimer.Tick -= FadeInTimer_Tick;
-                _fadeTimer.Stop();
-                _isFading = false;
-            }
-            else
-            {
-                this.Opacity += FADE_OPACITY_VALUE;
-            }
-        }
-
-        private void FadeOutTimer_Tick(object sender, EventArgs e)
-        {
-            if (this.Opacity <= 0)
-            {
-                _fadeTimer.Tick -= FadeOutTimer_Tick;
-                _fadeTimer.Stop();
-                _isFading = false;
-                if (((Action)_fadeTimer.Tag != null))
-                {
-                    ((Action)_fadeTimer.Tag).Invoke();
-                }
-
-            }
-            else
-            {
-                this.Opacity -= FADE_OPACITY_VALUE;
-            }
-        }
-
         private void button_Return_Click(object sender, EventArgs e)
         {
-            Action returnAction = new Action(() =>
-            {
-                this.Hide();
-                Program.loginForm.Show();
-                Program.loginForm.InitializeFadeIn();
-                this.Close();
-            });
-            InitializeFadeOut(returnAction);
         }
 
         private void Form_MouseDown(object sender, MouseEventArgs e)
         {
             if (!_isResizing && e.Button == MouseButtons.Left)
             {
-                if (gripRect.Contains(e.Location))
+                if (_gripRect.Contains(e.Location))
                 {
                     _isResizing = true;
                     _lastMousePosition = e.Location;
@@ -219,7 +69,7 @@ namespace Client.Forms
         {
             if (e.Button == MouseButtons.None && !_isResizing)
             {
-                if (gripRect.Contains(e.Location))
+                if (_gripRect.Contains(e.Location))
                 {
                     this.Cursor = Cursors.SizeNWSE;
                 }
@@ -238,7 +88,7 @@ namespace Client.Forms
                     _lastMousePosition = e.Location;
 
                     _borderRect = new Rectangle(ClientRectangle.Location, new Size(ClientRectangle.Width - 1, ClientRectangle.Height - 1));
-                    gripRect = new Rectangle(this.Width - GRIP_SIZE, this.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
+                    _gripRect = new Rectangle(this.Width - GRIP_SIZE, this.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
 
                     this.Invalidate();
                 }
@@ -286,7 +136,6 @@ namespace Client.Forms
             if (e.Button == MouseButtons.Left)
             {
                 _isDraging = false;
-                EnsureWindowPositionWithinScreenBounds();
             }
         }
         #endregion
@@ -302,7 +151,7 @@ namespace Client.Forms
             using (var p = new Pen(Colors.FormBorderColor))
             {
                 g.DrawRectangle(p, _borderRect);
-                g.FillRectangle(Brushes.Gray, gripRect);
+                g.FillRectangle(Brushes.Gray, _gripRect);
             }
         }
 
